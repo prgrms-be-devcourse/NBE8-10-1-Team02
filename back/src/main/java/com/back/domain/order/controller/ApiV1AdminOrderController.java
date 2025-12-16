7019273d8fc1f90fd1de2669f0f2b7e0cb0df3f0
suyeonTech <wsy-ofc@naver.com>
2025-12-16 12:25:38 +0900

    Chore: #5 불필요한 import 삭제

--- back/src/main/java/com/back/domain/order/controller/ApiV1AdminOrderController.java
+++ back/src/main/java/com/back/domain/order/controller/ApiV1AdminOrderController.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.Operation;
+
 
 import java.util.List;
 
