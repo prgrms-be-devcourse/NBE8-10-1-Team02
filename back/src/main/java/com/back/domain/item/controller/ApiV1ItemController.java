77f22fa56434a8943b8fb0c905c8890464dee73c
MADUPAL <kevin488@naver.com>
2025-12-16 15:27:44 +0900

    Feat: #15 상품(item)조회 기능 추가

--- back/src/main/java/com/back/domain/item/controller/ApiV1ItemController.java
+++ back/src/main/java/com/back/domain/item/controller/ApiV1ItemController.java
@@ -6,6 +6,7 @@ import com.back.domain.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,24 @@ import java.util.List;
 public class ApiV1ItemController {
     private ItemService itemService;
 
+    @GetMapping
+    @Transactional(readOnly = true)
+    public List<ItemDto> getItems() {
+        List<Item> items = itemService.findAll();
+
+        return items.stream()
+                .map(ItemDto::new)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public ItemDto getItem(
+            @PathVariable int id
+    ) {
+        Item item = itemService.findById(id).get();
+
+        return new ItemDto(item);
+    }
 
 }
--- back/src/main/java/com/back/domain/item/service/ItemService.java
+++ back/src/main/java/com/back/domain/item/service/ItemService.java
@@ -6,9 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private ItemRepository itemRepository;
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> findById(int id) {
+        return itemRepository.findById(id);
+    }
 }
